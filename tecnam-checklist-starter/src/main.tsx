5a4eb32acb9e50d15177ad68ebcd6fe348861f42
francis9891 <francis.morello@gmail.com>
2025-08-24 07:12:10 +1000

    Delete tecnam-checklist-starter directory

--- tecnam-checklist-starter/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import PreflightChecklist from "./components/PreflightChecklist";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <PreflightChecklist />
-  </React.StrictMode>
-);
