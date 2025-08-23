22a34f52080e24bf3f064737a9da39dd85132651
francis9891 <francis.morello@gmail.com>
2025-08-24 07:12:58 +1000

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PreflightChecklist from "./components/PreflightChecklist";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <PreflightChecklist />
+  </React.StrictMode>
+);
